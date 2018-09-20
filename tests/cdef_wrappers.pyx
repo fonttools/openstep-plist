6a3350993689b049cbbd520f2acde26de62c8dbf
Cosimo Lupo <cosimo@anthrotype.com>
2018-09-20 18:36:46 +0100

    add tests for line_number_strings func

--- tests/cdef_wrappers.pyx
+++ tests/cdef_wrappers.pyx
@@ -1,6 +1,7 @@
 from aplist._aplist cimport (
     _text,
     ParseInfo,
+    line_number_strings as c_line_number_strings,
     is_valid_unquoted_string_char as c_is_valid_unquoted_string_char,
 )
 from cpython.unicode cimport (
@@ -32,3 +33,12 @@ cdef class ParseContext:
 
 def is_valid_unquoted_string_char(Py_UNICODE c):
     return c_is_valid_unquoted_string_char(c)
+
+
+def line_number_strings(s, Py_ssize_t offset=0):
+    cdef ParseContext ctx = ParseContext.fromstring(s)
+    cdef ParseInfo *pi = &ctx.pi
+    if pi.begin != pi.curr and pi.begin + offset >= pi.end:
+        raise ValueError("offset past the end of string")
+    pi.curr += offset
+    return c_line_number_strings(pi)
--- tests/test_aplist.py
+++ tests/test_aplist.py
@@ -1,5 +1,19 @@
 from __future__ import absolute_import, unicode_literals
-from .cdef_wrappers import is_valid_unquoted_string_char
+from .cdef_wrappers import (
+    line_number_strings,
+    is_valid_unquoted_string_char,
+)
+
+
+def test_line_number_strings():
+    assert line_number_strings("", 0) == 1
+    assert line_number_strings("a\na", 1) == 1
+    assert line_number_strings("a\na", 2) == 2
+    assert line_number_strings("a\naa\n", 4) == 2
+    assert line_number_strings("a\naa\na", 5) == 3
+    assert line_number_strings("a\raa\ra", 5) == 3
+    assert line_number_strings("a\r\naa\ra", 6) == 3
+    assert line_number_strings("a\n\naa\n\na", 7) == 5
 
 
 def test_is_valid_unquoted_string_char():
