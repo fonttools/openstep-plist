f3bcd8f5561c5cce793da669c6088b7077bb2572
Cosimo Lupo <cosimo@anthrotype.com>
2018-09-21 12:25:46 +0100

    add travis and multibuild setup

---
+++ config.sh
@@ -0,0 +1,28 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # The function is called from an empty temporary directory.
+    cd ..
+
+    # Get absolute path to the pre-compiled wheel
+    wheelhouse=$(abspath wheelhouse)
+    wheel=$(ls ${wheelhouse}/aplist*.whl | head -n 1)
+    if [ ! -e "${wheel}" ]; then
+        echo "error: can't find wheel in ${wheelhouse} folder" 1>&2
+        exit 1
+    fi
+
+    # select tox environment based on the current python version
+    # E.g.: '2.7' -> 'py27'
+    TOXENV="py${MB_PYTHON_VERSION//\./}"
+
+    # Install pre-compiled wheel and run tests against it
+    tox --installpkg "${wheel}" -e "${TOXENV}"
+}
