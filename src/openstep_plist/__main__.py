ea8e215f4c9f1eb7e22cfd662161bf869778f953
Cosimo Lupo <cosimo@anthrotype.com>
2018-09-29 19:51:07 +0100

    test the dump function in __main__.py

--- src/openstep_plist/__main__.py
+++ src/openstep_plist/__main__.py
@@ -2,7 +2,6 @@
 
 import openstep_plist
 import json
-import base64
 import binascii
 # from collections import OrderedDict
 
@@ -29,6 +28,8 @@ def main(args=None):
     method = args[0]
     if method == "-a":
         parse = openstep_plist.load
+        dump = openstep_plist.dump
+
     elif method == "-g":
 
         def parse(fp, dict_type=dict):
@@ -37,6 +38,9 @@ def main(args=None):
             s = fp.read()
             p = Parser(current_type=dict_type)
             return p.parse(s)
+
+        from glyphsLib.writer import dump
+
     else:
         sys.exit("error: unknown option: %s" % method)
 
@@ -49,7 +53,8 @@ def main(args=None):
     if len(args) > 2:
         outfile = args[2]
         with open(outfile, "w", encoding="utf-8") as fp:
-            json.dump(data, fp, cls=BytesEncoder, sort_keys=True, indent="  ")
+            # json.dump(data, fp, cls=BytesEncoder, sort_keys=True, indent="  ")
+            dump(data, fp)
 
 
 if __name__ == "__main__":
