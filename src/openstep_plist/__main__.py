450223a7054095861e9811318c76766a0da92a1d
Cosimo Lupo <clupo@google.com>
2023-03-15 13:07:49 +0000

    __main__: add options for indent level, pager and json output

--- src/openstep_plist/__main__.py
+++ src/openstep_plist/__main__.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, unicode_literals
+import argparse
 import openstep_plist
 import json
 import binascii
+import pydoc
 from functools import partial
-from io import open
+from io import StringIO, open
 
 
 class BytesEncoder(json.JSONEncoder):
-
     def default(self, obj):
         from glyphsLib.types import BinaryData
 
@@ -23,15 +24,24 @@ def main(args=None):
 
         args = sys.argv[1:]
 
-    if len(args) < 2:
-        return 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", help="input file")
+    parser.add_argument("outfile", help="output file", default="-", nargs="?")
+    parser.add_argument(
+        "-g", "--glyphs", help="use glyphsLib parser/writer", action="store_true"
+    )
+    parser.add_argument(
+        "--no-pager", dest="pager", help="do not use pager", action="store_false"
+    )
+    parser.add_argument(
+        "-j", "--json", help="use json to serialize", action="store_true", default=False
+    )
+    parser.add_argument("-i", "--indent", help="indentation level", type=int, default=2)
+    args = parser.parse_args(args)
 
-    method = args[0]
-    if method == "-a":
-        parse = openstep_plist.load
-        dump = partial(openstep_plist.dump, indent=0)
-
-    elif method == "-g":
+    if not args.glyphs:
+        parse = partial(openstep_plist.load, use_numbers=True)
+    else:
 
         def parse(fp, dict_type=dict):
             from glyphsLib.parser import Parser
@@ -40,20 +50,28 @@ def main(args=None):
             p = Parser(current_type=dict_type)
             return p.parse(s)
 
-        from glyphsLib.writer import dump
-
+    if args.json:
+        dump = partial(
+            json.dump, cls=BytesEncoder, sort_keys=True, indent=" " * args.indent
+        )
     else:
-        sys.exit("error: unknown option: %s" % method)
+        if args.glyphs:
+            from glyphsLib.writer import dump
+        else:
+            dump = partial(openstep_plist.dump, indent=args.indent)
 
-    infile = args[1]
-
-    with open(infile, "r", encoding="utf-8") as fp:
+    with open(args.infile, "r", encoding="utf-8") as fp:
         data = parse(fp)
 
-    if len(args) > 2:
-        outfile = args[2]
-        with open(outfile, "w", encoding="utf-8") as fp:
-            # json.dump(data, fp, cls=BytesEncoder, sort_keys=True, indent="  ")
+    if args.outfile == "-":
+        if args.pager:
+            buf = StringIO()
+            dump(data, buf)
+            pydoc.pager(buf.getvalue())
+        else:
+            dump(data, sys.stdout)
+    else:
+        with open(args.outfile, "w", encoding="utf-8") as fp:
             dump(data, fp)
 
 
